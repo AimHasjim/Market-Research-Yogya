14be765473fa4a453835a9185f57db90f4d860e1
Wan Hasjim <aimhasjim@gmail.com>
2023-08-15 20:26:10 +0700

    Commit Heroku

--- init.r
+++ init.r
@@ -1,7 +1,7 @@
 my_packages <- c("tidyverse", "readxl","plotly","sf","shiny","shinydashboard","tmap","leaflet")
 
 install_if_missing <- function(p){
-  if (p %in% rownmes(installed.packages()) == F) {
+  if (p %in% rownames(installed.packages()) == F) {
     install.packages(p)
   }
 }
